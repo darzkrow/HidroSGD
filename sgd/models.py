2e7c6d2518343cbc97070c9502f59a768258b582
Frank Dev <gfrancojf@gmail.com>
2023-12-13 22:12:54 -0400

    Creando Modelo de SGD_ENV

--- sgd/models.py
+++ sgd/models.py
@@ -1,93 +1,49 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
-
-class Enviado(models.Model):
-    uuid = models.UUIDField()
-    fecha_doc = models.DateField()
-    referencia = models.CharField(max_length=255)
-    tipo = models.CharField(max_length=100)
-    destinatario = models.CharField(max_length=255)
-    estado = models.CharField(max_length=100, null=True, default='NO ESPECIFICADO')
-    otro = models.CharField(max_length=255)
-    estatus = models.CharField(max_length=100)
-    fecha_rec = models.DateField()
-    file = models.CharField(max_length=255)
-    nro_doc = models.CharField(max_length=255, null=True, default='0')
-    tipo_doc = models.CharField(max_length=100)
-    recibido_por = models.CharField(max_length=255)
-    accion = models.CharField(max_length=100)
-    bandeja_de = models.CharField(max_length=100)
-    seguimiento = models.CharField(max_length=255)
-    active = models.IntegerField(null=True, default=1)
-
-    codigo = models.CharField(max_length=255)
-    tipo_c = models.IntegerField()
-
-    # Extras
-    ref_doc = models.CharField(max_length=255, null=True)
-    fecha_lim = models.DateField(null=True)
-    ccopia = models.CharField(max_length=255, null=True)
-
-    # Definir las relaciones con los usuarios si es necesario
-    # id_user = models.ForeignKey(User, on_delete=models.CASCADE)
-    # id_user_destino = models.ForeignKey(User, on_delete=models.CASCADE)
-
+class Correspondence(models.Model):
+    title = models.CharField('Titulo',max_length=100)
+    cod = models.CharField('Codigo', max_length=50, null=False, blank=False)
+    
+    content = models.TextField('Descripción', max_length=255)
+    created_by = models.ForeignKey(User, on_delete=models.CASCADE, related_name='created_correspondence')
+    tracked_by = models.ForeignKey(User, on_delete=models.CASCADE, related_name='tracked_correspondence', null=True, blank=True)
+    tracked_at = models.DateTimeField(null=True, blank=True)
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
 
-    class Meta:
-        db_table = 'enviados'  # Nombre de la tabla en la base de datos
+    # Agregar campo para el estado de la correspondencia
+    STATUS_CHOICES = [
+        ('PENDING', 'Pending'),
+        ('IN_PROGRESS', 'In Progress'),
+        ('COMPLETED', 'Completed'),
+        ('ON_HOLD', 'On Hold'),
+    ]
+    status = models.CharField(max_length=20, choices=STATUS_CHOICES, default='PENDING')
 
+    def __str__(self):
+        return self.subject
 
-class Recibido(models.Model):
-    tipo = models.CharField(max_length=100)
-    uuid = models.UUIDField()
-    remitente = models.CharField(max_length=255)
-    estado = models.CharField(max_length=100, null=True, default='NO ESPECIFICADO')
-    otro = models.CharField(max_length=255)
-    recibido_por = models.CharField(max_length=255)
-    referencia = models.CharField(max_length=255)
-    file = models.CharField(max_length=255)
-    tipo_doc = models.CharField(max_length=100)
-    fecha_doc = models.DateField()
-    nro_doc = models.CharField(max_length=255)
-    estatus = models.CharField(max_length=100)
-    accion = models.CharField(max_length=100)
-    seguimiento = models.CharField(max_length=255)
-    active = models.IntegerField(null=True, default=1)
-
-    bandeja_de = models.CharField(max_length=100)
-    codigo = models.CharField(max_length=255)
-    tipo_c = models.IntegerField()
-
-    # Extras
-    ref_doc = models.CharField(max_length=255, null=True)
-    fecha_lim = models.DateField(null=True)
-    # id_user_destino = models.ForeignKey(User, on_delete=models.CASCADE)
-
+class Comment(models.Model):    
+    correspondence = models.ForeignKey(Correspondence, on_delete=models.CASCADE, related_name='comments')
+    author = models.ForeignKey(User, on_delete=models.CASCADE)
+    text = models.TextField()
     created_at = models.DateTimeField(auto_now_add=True)
-    updated_at = models.DateTimeField(auto_now=True)
 
-    class Meta:
-        db_table = 'recibidos'  # Nombre de la tabla en la base de datos
-
+    def __str__(self):
+        return f"Comment by {self.author.username} on {self.correspondence.subject}"
 
 
 
-class Seguimiento(models.Model):
-    id_documento = models.IntegerField()
-    id_usuario = models.IntegerField()
-    accion = models.CharField(max_length=255)
-    fecha = models.DateField()
-    bandeja_de = models.IntegerField()
-    instruccion = models.CharField(max_length=255)
-    estatus = models.CharField(max_length=255)
-    tipo_c = models.IntegerField()
-    # seguimiento = models.CharField(max_length=255)  # Si es necesario agregar este campo
 
-    created_at = models.DateTimeField(auto_now_add=True)
-    updated_at = models.DateTimeField(auto_now=True)
+class CorrespondenceAssignment(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    correspondence = models.ForeignKey(Correspondence, on_delete=models.CASCADE)
+    assigned_at = models.DateTimeField(auto_now_add=True)
 
-    class Meta:
-        db_table = 'seguimientos' 
+    # Otros campos si es necesario
+
+    def __str__(self):
+        return f"{self.user.username} - {self.correspondence.subject}"
+
+
